cd93750b192008adf420928ae20809e357d7d03b
ANONYMOUSx46 <liamjudedewet85@gmail.com>
2025-02-25 14:34:07 +0200

    Update SignIn.tsx

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { auth } from "../firebase"; // Import your Firebase setup
+import { auth } from "../firebase"; // Import  Firebase setup
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
